ddf97b6f49d0bd58acb45c7a05d0ce7d5bb4aa81
Marcel Rico <personal.mr4@gmail.com>
2019-12-16 01:37:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,6 @@
 class Dog
-  def initialize(dogs_name)
+  def initialize(dogs_name,breed)
     @name = dogs_name
-  end
-  
-  def breed=(breed)
     @breed = breed
-  end
-  
-  def breed
-    @breed
   end
 end
